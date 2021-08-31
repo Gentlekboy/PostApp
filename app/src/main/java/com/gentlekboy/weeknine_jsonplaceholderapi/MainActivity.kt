7005c394095ff62af2fc0a8f3d8b7351305981e7
gentlekboy <gentlekboy@gmail.com>
2021-08-31 01:56:14 +0100

    added buttons in main activity for navigation to different implementations

--- app/src/main/java/com/gentlekboy/weeknine_jsonplaceholderapi/MainActivity.kt
+++ app/src/main/java/com/gentlekboy/weeknine_jsonplaceholderapi/MainActivity.kt
@@ -1,11 +1,21 @@
 package com.gentlekboy.weeknine_jsonplaceholderapi
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.gentlekboy.weeknine_jsonplaceholderapi.databinding.ActivityMainBinding
+import com.gentlekboy.weeknine_jsonplaceholderapi.firstimplementation.ui.PostActivity
+
+private lateinit var binding: ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.firstImplementationButton.setOnClickListener {
+            startActivity(Intent(this, PostActivity::class.java))
+        }
     }
 }
