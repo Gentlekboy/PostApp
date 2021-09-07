093ca00435d5900742f68f0c9c9d2d8be274d81e
gentlekboy <gentlekboy@gmail.com>
2021-09-07 05:35:12 +0100

    suppressed unchecked cast on line 10

--- app/src/main/java/com/gentlekboy/weeknine_jsonplaceholderapi/firstimplementation/viewmodel/MainViewModelFactory.kt
+++ app/src/main/java/com/gentlekboy/weeknine_jsonplaceholderapi/firstimplementation/viewmodel/MainViewModelFactory.kt
@@ -5,8 +5,8 @@ import androidx.lifecycle.ViewModelProvider
 import com.gentlekboy.weeknine_jsonplaceholderapi.firstimplementation.repository.Repository
 
 class MainViewModelFactory(private val repository: Repository): ViewModelProvider.Factory {
+    @Suppress("UNCHECKED_CAST")
     override fun <T : ViewModel?> create(modelClass: Class<T>): T {
         return MainViewModel(repository) as T
     }
-
 }
