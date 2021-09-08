592e250254235b1ae43cc8071ceca27b2df37b51
gentlekboy <gentlekboy@gmail.com>
2021-09-08 10:03:45 +0100

    added post activity for MVC implementation

---
+++ app/src/main/java/com/gentlekboy/weeknine_jsonplaceholderapi/secondimplementation/ui/MvcPostActivity.kt
@@ -0,0 +1,12 @@
+package com.gentlekboy.weeknine_jsonplaceholderapi.secondimplementation.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.gentlekboy.weeknine_jsonplaceholderapi.R
+
+class MvcPostActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_mvc_post)
+    }
+}
