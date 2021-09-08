b7ea0644513d64d7e7b745efb52206545959648e
gentlekboy <gentlekboy@gmail.com>
2021-09-08 10:02:11 +0100

    created data classes for fetching posts from the post endpoint

---
+++ app/src/main/java/com/gentlekboy/weeknine_jsonplaceholderapi/secondimplementation/model/data/posts/MvcPostItems.kt
@@ -0,0 +1,8 @@
+package com.gentlekboy.weeknine_jsonplaceholderapi.secondimplementation.model.data.posts
+
+data class MvcPostItems(
+    val body: String,
+    val id: Int,
+    val title: String,
+    val userId: Int
+)
---
+++ app/src/main/java/com/gentlekboy/weeknine_jsonplaceholderapi/secondimplementation/model/data/posts/MvcPosts.kt
@@ -0,0 +1,3 @@
+package com.gentlekboy.weeknine_jsonplaceholderapi.secondimplementation.model.data.posts
+
+class MvcPosts: ArrayList<MvcPostItems>()
