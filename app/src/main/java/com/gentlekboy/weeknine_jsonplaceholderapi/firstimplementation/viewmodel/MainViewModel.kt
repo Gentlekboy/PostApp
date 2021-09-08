46f81af0dc75fcb742863a09a8aebdc1944fd9f7
gentlekboy <gentlekboy@gmail.com>
2021-09-08 10:01:05 +0100

    moved API calls to background thread

--- app/src/main/java/com/gentlekboy/weeknine_jsonplaceholderapi/firstimplementation/viewmodel/MainViewModel.kt
+++ app/src/main/java/com/gentlekboy/weeknine_jsonplaceholderapi/firstimplementation/viewmodel/MainViewModel.kt
@@ -1,5 +1,6 @@
 package com.gentlekboy.weeknine_jsonplaceholderapi.firstimplementation.viewmodel
 
+import android.util.Log
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
@@ -7,43 +8,64 @@ import com.gentlekboy.weeknine_jsonplaceholderapi.firstimplementation.model.data
 import com.gentlekboy.weeknine_jsonplaceholderapi.firstimplementation.model.data.posts.PostItems
 import com.gentlekboy.weeknine_jsonplaceholderapi.firstimplementation.model.data.posts.Posts
 import com.gentlekboy.weeknine_jsonplaceholderapi.firstimplementation.repository.Repository
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 import retrofit2.Response
 
 class MainViewModel(private val repository: Repository): ViewModel() {
     val allPosts: MutableLiveData<Response<Posts>> = MutableLiveData()
     val allComments: MutableLiveData<Response<Comments>> = MutableLiveData()
-//    val pushPost: MutableLiveData<Response<PostItems>> = MutableLiveData()
     val pushPost2: MutableLiveData<Response<PostItems>> = MutableLiveData()
 
     //Save fetched posts to livedata
     fun fetchPosts(){
-        viewModelScope.launch {
-            val response = repository.getPost()
-            allPosts.value = response
+        viewModelScope.launch{
+            withContext(Dispatchers.IO){
+                try {
+                    val response = repository.getPost()
+
+                    withContext(Dispatchers.Main){
+                        allPosts.value = response
+                    }
+                }catch (e: Exception){
+                    Log.d("GKB", "fetchPosts: ${e.message}")
+                }
+            }
         }
     }
 
     //Save fetched comments to livedata
     fun fetchComments(id: String){
         viewModelScope.launch {
-            val response = repository.getComment(id)
-            allComments.value = response
+            withContext(Dispatchers.IO){
+                try {
+                    val response = repository.getComment(id)
+
+                    withContext(Dispatchers.Main){
+                        allComments.value = response
+                    }
+                }catch (e: Exception){
+                    Log.d("GKB", "fetchPosts: ${e.message}")
+                }
+            }
         }
     }
-
-//    fun pushPost(post: PostItems){
-//        viewModelScope.launch {
-//            val response = repository.pushPost(post)
-//            pushPost.value = response
-//        }
-//    }
 
     //Save posted response to livedata
     fun makeAPostToApi(userId: Int, id: Int, title: String, body: String){
         viewModelScope.launch {
-            val response = repository.pushPost2(userId, id, title, body)
-            pushPost2.value = response
+            withContext(Dispatchers.IO){
+                try{
+                    val response = repository.pushPost2(userId, id, title, body)
+
+                    withContext(Dispatchers.Main){
+                        pushPost2.value = response
+                    }
+                }catch (e: Exception){
+                    Log.d("GKB", "fetchPosts: ${e.message}")
+                }
+            }
         }
     }
 }
