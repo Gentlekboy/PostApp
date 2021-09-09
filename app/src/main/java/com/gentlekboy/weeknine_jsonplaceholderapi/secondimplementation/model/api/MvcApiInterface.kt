8c620283584bf11d5506b7a728607a2a2676352a
gentlekboy <gentlekboy@gmail.com>
2021-09-09 10:23:30 +0100

    added a function that makes a post request

--- app/src/main/java/com/gentlekboy/weeknine_jsonplaceholderapi/secondimplementation/model/api/MvcApiInterface.kt
+++ app/src/main/java/com/gentlekboy/weeknine_jsonplaceholderapi/secondimplementation/model/api/MvcApiInterface.kt
@@ -1,19 +1,31 @@
 package com.gentlekboy.weeknine_jsonplaceholderapi.secondimplementation.model.api
 
 import com.gentlekboy.weeknine_jsonplaceholderapi.secondimplementation.model.data.comments.MvcComments
+import com.gentlekboy.weeknine_jsonplaceholderapi.secondimplementation.model.data.posts.MvcPostItems
 import com.gentlekboy.weeknine_jsonplaceholderapi.secondimplementation.model.data.posts.MvcPosts
 import retrofit2.Call
 import retrofit2.Response
+import retrofit2.http.Body
 import retrofit2.http.GET
+import retrofit2.http.POST
 import retrofit2.http.Path
 
 interface MvcApiInterface {
+    //Get request for posts
     @GET("posts")
     fun getPost(): Call<MvcPosts>
 
+    //Get request for comments
     @GET("posts/{id}/comments")
     fun getComments(
         @Path("id")
         id: String
     ): Call<MvcComments>
+
+    //Make post request as JSON format
+    @POST("posts")
+    fun makeAPost(
+        @Body
+        post: MvcPostItems
+    ): Call<MvcPostItems>
 }
