3443490738f7ad192cf31d358ff8597b6edc81c8
gentlekboy <gentlekboy@gmail.com>
2021-09-09 08:07:46 +0100

    added a function to make a get comment request in interface

--- app/src/main/java/com/gentlekboy/weeknine_jsonplaceholderapi/secondimplementation/model/api/MvcApiInterface.kt
+++ app/src/main/java/com/gentlekboy/weeknine_jsonplaceholderapi/secondimplementation/model/api/MvcApiInterface.kt
@@ -1,11 +1,19 @@
 package com.gentlekboy.weeknine_jsonplaceholderapi.secondimplementation.model.api
 
+import com.gentlekboy.weeknine_jsonplaceholderapi.secondimplementation.model.data.comments.MvcComments
 import com.gentlekboy.weeknine_jsonplaceholderapi.secondimplementation.model.data.posts.MvcPosts
 import retrofit2.Call
 import retrofit2.Response
 import retrofit2.http.GET
+import retrofit2.http.Path
 
 interface MvcApiInterface {
     @GET("posts")
     fun getPost(): Call<MvcPosts>
+
+    @GET("posts/{id}/comments")
+    fun getComments(
+        @Path("id")
+        id: String
+    ): Call<MvcComments>
 }
